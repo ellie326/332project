70f3080ac722419760213688b119f9bbd88e9045
cjw7656 <136216206+cjw7656@users.noreply.github.com>
2024-12-02 06:43:23 +0900

    week 7 update

--- sdproject/master/src/main/scala/master.scala
+++ sdproject/master/src/main/scala/master.scala
@@ -1,15 +1,62 @@
-import io.grpc.{Server, ServerBuilder}
-import scala.concurrent.{ExecutionContext, Future}
+import io.grpc.{Server, ServerBuilder, ManagedChannel, ManagedChannelBuilder}
 import master.MasterGrpc
-import master.{RegisterRequest, RegisterResponse}
+import worker.WorkerGrpc
+import master.{RegisterRequest, RegisterResponse, ShuffleCompleteRequest, ShuffleCompleteResponse, MergeSortCompleteRequest, MergeSortCompleteResponse}
+import worker.{ShuffleStartRequest, ShuffleStartResponse, MergeSortStartRequest, MergeSortStartResponse}
+import com.typesafe.scalalogging.LazyLogging
+import scala.concurrent.{Promise, Future, ExecutionContext, Await}
+import scala.concurrent.duration.Duration
+import scala.concurrent.blocking
+import com.google.protobuf.ByteString
+import java.util.concurrent.ConcurrentLinkedQueue
+import scala.jdk.CollectionConverters._
+import java.net.InetAddress
+
+
 
 object Master extends App {
   private val ip: String = "141.223.16.227"
   private val port: Int = 7777
-  private val workerNum: Int = 3  // 예시로 3명의 워커가 등록되면 시작
+  private val workerNum: Int = args(0).toInt
+  private val registerRequests: ConcurrentLinkedQueue[RegisterRequest] = new ConcurrentLinkedQueue[RegisterRequest]()
 
-  // 워커 등록을 위한 리스트
-  private var workerIps: List[String] = List()
+  private val CompleteAllRegister : Promise[Unit] = Promise()
+
+  private val workerData: Future[(List[String], List[ByteString])] = getWorkerData
+
+  private val workerIps: Future[List[String]] = workerData.map(_._1)
+  private val ranges: Future[List[ByteString]] = workerData.map(_._2)
+
+  private val workerChannels : Future[List[ManagedChannel]] = async{
+    val ip_ = await(workerIps)
+
+    def createChannel(ip: String): ManagedChannel = {
+      ManagedChannelBuilder.forAddress(ip,port).
+        usePlaintext().
+        asInstanceOf[ManagedChannelBuilder[_]].
+        build()
+    }
+
+    val channels = ips.map(createChannel)
+
+    channels
+  }
+
+  sendShuffleStart
+
+  private val shuffleCompleteRequests: ConcurrentLinkedQueue[String] = new ConcurrentLinkedQueue[String]()
+  private val CompleteAllShuffle: Promise[Unit] = Promise()
+  private val shuffleCompletedIp: Future[List[String]] =  getShuffleCompletedIps
+
+  sendMergesortStart
+
+  private val sortCompleteRequests: ConcurrentLinkedQueue[MergeSortCompleteRequest] = new ConcurrentLinkedQueue[MergeSortCompleteRequest]()
+  private val CompleteAllMergesort: Promise[Unit] = Promise()
+
+
+  
+  
+
 
   // gRPC 서버 설정 및 시작
   private val server = ServerBuilder
@@ -18,28 +65,101 @@ object Master extends App {
     .build
     .start
 
-  println(s"$ip:$port")
-
-  // 서버 종료 시 클린업
-  sys.addShutdownHook {
-    println("Shutting down server...")
-    server.shutdown()
+  blocking{
+    Await.result(CompleteAllMergesort.future, Duration.Inf)
   }
+  blocking{
+    Await.result(workerChannels,Duration.Inf).foreach(_.shutdown())
+  }
+  server.shutdown()
+  private val result: List[MergeSortCompleteRequest]= sortCompleteRequests.asScala.toList
 
-  // 서버 계속 실행
-  server.awaitTermination()
+
+
 
   // gRPC 서비스 구현
   private class MasterImpl extends MasterGrpc.Master {
     override def register(request: RegisterRequest): Future[RegisterResponse] = {
-      println(s"Register request from ${request.ip}")
-      workerIps = (workerIps :+ request.ip).distinct.sorted  // 워커 IP 등록
-
-      // 모든 워커가 등록되면 워커 IP 리스트 출력
-      if (workerIps.length == workerNum) {
-        println(s"Sorted Worker IPs: ${workerIps.mkString(", ")}")
-      }
-      Future.successful(RegisterResponse(ip = ip))  // 응답 반환
+      logRequest(request)
+      saveRequest(request)
+      isComplete()
+      buildResponse()
     }
+
+    private def logRequest(request:RegisterRequest): Unit = {
+      logger.info(s"Register received request from ${request.ip}")
+    }
+    
+    private def saveRequest(request:RegisterRequest): Unit = {
+      registerRequests.add(request)
+    }
+
+    private def isComplete(): Unit = {
+      if(registerRequests.size == workerNum){
+        CompleteAllRegister.trySuccess(())
+      }
+    }
+
+    private def buildResponse():Unit={
+      Future.successful(RegisterResponse(ip=ip))
+    }
+
+    override def shuffleComplete(request: ShuffleCompleteRequest): Future[ShuffleCompleteResponse] = {
+      logger.info(s"Distribute recieved complete request from ${request.ip}")
+      shuffleCompleteRequests.add(request.ip)
+      if(shuffleCompleteRequests.size == workerNum){
+        CompleteAllShuffle trySuccess()
+      }
+      Future(ShuffleCompleteResponse())
+    }
+
+    override def mergeSortComplete(request: MergeSortCompleteRequest): Future[MergeSortCompleteResponse] = {
+      sortCompleteRequests.add(request)
+      if(sortCompleteRequests.size == workerNum){
+        CompleteAllMergesort trySuccess()
+      }
+      Future(MergeSortCompleteResponse())
+    }
+  }
+
+  private def getWorkerData: Future[(List[String],List[ByteString])] = async{
+    await(CompleteAllRegister.future)
+
+    val SortedRequests = registerRequests.asScala.toList.sortBy(_.ip)
+
+    val workerIps = SortedRequests.map(_.ip)
+
+    val keys = SortedRequests.flatMap(_.samples.map(_.key))
+
+    val rangeSize = math.ceil(keys.length.toDouble/workerNum).toInt
+    val ranges = keys.sorted.grouped(rangeSize).map(_.head).toList
+
+    (workerIps,ranges)
+  }
+
+  private def sendShuffleStart: Future[Unit] = async{
+    val workerIps = await(this.workerIps)
+    val ranges = await(this.ranges)
+    val channels = await(this.workerChannels)
+    val IpRange : Map[String, ByteString] = (workerIps zip ranges).toMap
+    val blockingStubs: List[WorkerGrpc.WorkerBlockingStub] = channels map WorkerGrpc.blockingStub
+    val requests: ShuffleStartRequest = ShuffleStartRequest(ranges = IpRange)
+    val responses: List[ShuffleStartResponse] = blockingStubs map(_.StartShuffle(request)) 
+    ()
+  }
+
+  private def getShuffleCompletedIps: Future[List[String]] =async {
+    await(CompleteAllShuffle.future)
+    val ips = shuffleCompleteRequests.asScala.toList.sorted
+    ips
+  }
+
+  private def sendMergesortStart = async {
+    val workerIps = await(shuffleCompletedIp)
+    val channels = await(workerChannels)
+    val blockingStubs: List[WorkerGrpc.WorkerBlockingStub] = channels map WorkerGrpc.blockingStub
+    val request: MergeSortStartRequest = MergeSortStartRequest()
+    val responses: List[MergeSortStartResponse] = blockingStubs map(_.MergeSortStart(request))
+    ()
   }
 }

96f3a4da087e29889f81f48e032b609bccd97727
ellie326 <137602866+ellie326@users.noreply.github.com>
2024-12-07 08:11:53 +0900

    Update DataProcess.scala

--- sdproject/worker/src/main/scala/DataProcess.scala
+++ sdproject/worker/src/main/scala/DataProcess.scala
@@ -1,38 +1,59 @@
-//worker가 신호 받으면 이 파일의 함수를 호출해서 sorting, shuffle등 진행
-package worker
-import java.nio.file.{Files.Paths,Path}
-import java.io.File
-import java.util.concurrent.atomic.AtomicInteger
+package com.orange.worker
 
-import scala.jdk.CollectionConverters._
-import scala.collection.mutable.ListBuffer
+import com.orange.bytestring_ordering.ByteStringOrdering._
+
+import java.util.concurrent.atomic.AtomicInteger
+import java.io.{File, FileWriter}
+import java.nio.file.{Files, Paths, StandardOpenOption}
 import scala.annotation.tailrec
+import scala.io._
+import scala.concurrent.{ExecutionContext, Future, Promise, blocking}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.async.Async.{async, await}
+import com.google.protobuf.ByteString
+import com.orange.bytestring_ordering.ByteStringOrdering._
+import scala.math.Ordered.orderingToOrdered
+
+import com.orange.proto.data._
+import com.typesafe.scalalogging.Logger
+
+
+import scala.collection.mutable.ListBuffer
+import scala.util.Using
 
 
 
 
 class DataProcess(InputDirectories: List[String],OutputDirectory: String){
+    private val logger: Logger = Logger("DataProcess")
+
+    logger.info(s"Data Process initiated")
+    logger.info(s"Input Directory passed: $InputDirectories")
+    logger.info(s"Output Directory passed: $OutputDirectory")
+
     private val SortedInputDirectory: String = s"$OutputDirectory/tmp1"
     private val DistributedDirectory: String = s"$OutputDirectory/tmp2"
-    private val directoryHistory: Array[(String, AtomicInteger)]= Array(
+    private val directoryHistory: Map[String, AtomicInteger]= Map(
         SortedInputDirectory -> new AtomicInteger(0),
         DistributedDirectory -> new AtomicInteger(0),
         OutputDirectory      -> new AtomicInteger(0)
     )
-
-    private val InputPaths: List[String] = InputDirectories.map(getPaths).flatten
-    private val SortInputData: Future[List[Unit]] = Future.traverse(InputPaths){ path=> 
-        Future{x`
-            InternalSortComplete(path,SortedInputDirectory)
-        }
-    }
 
     //Initialize directory
     InitializeDirectory(OutputDirectory)
     InitializeDirectory(SortedInputDirectory)
     InitializeDirectory(DistributedDirectory)
 
+    private val InputPaths: List[String] = InputDirectories.map(getPaths).flatten
+    private val SortInputData: Future[List[Unit]] = Future.traverse(InputPaths){ path=> 
+        Future{
+            InternalSortComplete(path,SortedInputDirectory)
+        }
+    }
 
+
+    logger.info(s"Input Directory Initialized: $SortedInputDirectory")
+    logger.info(s"Output Directory Initialized: $DistributedDirectory")
 
 
     private def InitializeDirectory(Directory: String): Unit = {
@@ -92,20 +113,29 @@ class DataProcess(InputDirectories: List[String],OutputDirectory: String){
     */
 
     def getSamplesFromUnsorted: Future[List[Data]] = async {
+        logger.info(s"get samples from unsorted")
         await(SortInputData)
+        logger.info(s"after await")
         if (InputPaths.isEmpty) {
+            logger.info(s"Input path is empty")
             List()
         } else {
+            logger.info(s"Sampling Starts from ${InputPaths.head}")
             val (fileContent, dataIterator) = getData(InputPaths.head)
             val samples = Using(fileContent) { _ =>
             dataIterator.take(DataConfig.num_sample).toList
             }.getOrElse(Nil)
+            logger.info(s"Sampling Completed. ${samples.length} sampels had been considered")
             samples
         }
     }
 
+    def saveDistributedData(records: Seq[Data]): Unit = {
+        saveData(DistributedDirectory, records)
+    }
+
     //file에서 몇개의 sample들을 추출, 뭔가 file에서 가져오면 string 형태가 될텐데 이걸 Data structure에 맞게 변형하는 함수가 필요할듯
-    private def getData(filelName: String): (BufferedSource, Iterator[Data])={
+    private def getData(fileName: String): (BufferedSource, Iterator[Data])={
         val fileContent: BufferedSource = scala.io.Source.fromFile(fileName,"ISO-8859-1")
         val dataIterator: Iterator[Data] = fileContent.getLines()
             .flatMap{line => 
@@ -118,7 +148,7 @@ class DataProcess(InputDirectories: List[String],OutputDirectory: String){
     //string을 Data에 맞게 변형
     private def ConvertToData(str: String): Data={
         val EntireByte: Array[Byte] = str.getBytes("ISO-8859-1")
-        val key: ByteString = ByteString.copyFrom(EntireByte.take(DataConfig.leng th_key))
+        val key: ByteString = ByteString.copyFrom(EntireByte.take(DataConfig.length_key))
         val value: ByteString = ByteString.copyFrom(EntireByte.drop(DataConfig.length_key))
         Data(key,value)
     }
@@ -149,32 +179,30 @@ class DataProcess(InputDirectories: List[String],OutputDirectory: String){
 
 
     private def saveData(directory: String, data: Seq[Data]): Unit={
-        val directoryEntry = directoryHistory.find(_._1 == directory).getOrElse{
-            throw new IllegalArgumentException(s"Directory $directory not found in directories array: ${directories.mkString(", ")}")
-        }
 
-        val (path, counter) = directoryEntry
-        val num : Int = counter.getAndIncrement()
-        val file: File = new File(path + File.seperator + f"partition$num%010d")
-        val ArrayData : Array[Byte] = Data.flatMap(Data => Data.key.toByteArray ++ Data.value.toByteArray).toArray
+        val num : Int = directoryHistory(directory).getAndIncrement()
+        val file: File = new File(directory + File.separator + f"partition$num%010d")
+        val ArrayData : Array[Byte] = data.flatMap(Data => Data.key.toByteArray ++ Data.value.toByteArray).toArray
 
         Files.write(Paths.get(file.getPath), ArrayData)
     }
+
     def MergeSortandSave(): Unit = {
         logger.info(s"Merge Sort Distributed Files")
 
+
         val distributedDataDirectory = getPaths(DistributedDirectory)
-        val (bufferedSourceList: List[BufferedSource], IteratorList: List[Iterator[Record]]) =
+        val (bufferedSourceList: List[BufferedSource], iteratorList: List[Iterator[Data]]) =
         (distributedDataDirectory map getData).unzip
 
         //merge sort 진행 
         logger.info(s"Merge Sort starts")
-        val MergeSortResult: Iterator[Record] = MergeSort(IteratorList)
+        val MergeSortResult: Iterator[Data] = MergeSort(iteratorList)
         logger.info(s"Merge Sort completed")
 
         //merge sort 결과 block 단위로 저장 
-        val SaveMergeSortResult: Iterator[List[Record]] =
-        MergeSortResult.grouped(RecordConfig.WritableUnit) map (_.toList)
+        val SaveMergeSortResult: Iterator[List[Data]] =
+        MergeSortResult.grouped(DataConfig.writeBlockSize) map (_.toList)
 
         try {
             SaveMergeSortResult foreach (records => saveData(OutputDirectory, records))
@@ -183,56 +211,56 @@ class DataProcess(InputDirectories: List[String],OutputDirectory: String){
         }
     }
 
-    private def MergeSort (IteratorList: List[Iterator[Record]]): Iterator[Record] = {
+    private def MergeSort (iteratorList: List[Iterator[Data]]): Iterator[Data] = {
         
-        val (LeftIterator: List[Iterator[Record]], RightIterator: List[Iterator[Record]]) =
-        DividePart(IteratorList, List(), List())
+        val (leftIterator: List[Iterator[Data]], rightIterator: List[Iterator[Data]]) =
+        DividePart(iteratorList, List(), List())
 
-        (LeftIterator, RightIterator) match {
+        (leftIterator, rightIterator) match {
         case (Nil, Nil) => Iterator() //빈 Iterator 반환 
 
         case (_, Nil) =>
-            if (LeftIterator.length != 1) {
+            if (leftIterator.length != 1) {
                 throw new IllegalStateException("LeftIterator must have exactly one element")
             }
-            LeftIterator.head
+            leftIterator.head
 
 
         case (_, _) =>
-            val (LeftIteratorSorted: Iterator[Record], RightIteratorSorted: Iterator[Record]) =
-            (MergeSort(LeftIterator), MergeSort(RightIterator))
+            val (leftIteratorSorted: Iterator[Data], rightIteratorSorted: Iterator[Data]) =
+            (MergeSort(leftIterator), MergeSort(rightIterator))
             
-            ConquerPart(LeftIteratorSorted, RightIteratorSorted)
+            ConquerPart(leftIteratorSorted, rightIteratorSorted)
         }
     }
 
     @tailrec
-    private DividePart(IteratorList: List[Iterator[Record]], LeftList: List[Iterator[Record]], RightList: List[Iterator[Record]]): 
-    (List[Iterator[Record]], List[Iterator[Record]]) = {
+    private def DividePart(iteratorList: List[Iterator[Data]], leftList: List[Iterator[Data]], rightList: List[Iterator[Data]]): 
+    (List[Iterator[Data]], List[Iterator[Data]]) = {
 
-        IteratorList match {
-            case Nil => (LeftList, RightList)
-            case HeadofIterator :: Nil => (HeadofIterator :: LeftList, RightList)
-            case FirstIterator :: SecondIterator :: Remainder => DividePart(Remainder, FirstIterator :: LeftList, SecondIterator :: RightList)
+        iteratorList match {
+            case Nil => (leftList, rightList)
+            case headofIterator :: Nil => (headofIterator :: leftList, rightList)
+            case firstIterator :: secondIterator :: remainder => DividePart(remainder, firstIterator :: leftList, secondIterator :: rightList)
         }
     }
 
 
-    private ConquerPart(LeftIteratorSorted: Iterator[Record], RightIteratorSorted: Iterator[Record]): Iterator[Record] = {
+    private def ConquerPart(leftIteratorSorted: Iterator[Data], rightIteratorSorted: Iterator[Data]): Iterator[Data] = {
 
-        val hasNextLeft: Boolean = LeftIteratorSorted.hasNext
-        val leftElement: Option[Record] = 
+        val hasNextLeft: Boolean = leftIteratorSorted.hasNext
+        val leftElement: Option[Data] = 
             if (hasNextLeft) {
-                val element: Record = LeftIteratorSorted.next()
+                val element: Data = leftIteratorSorted.next()
                 Some(element)
             } else {
                 None
             }
 
-        val hasNextRight: Boolean = RightIteratorSorted.hasNext
-        val rightElement: Option[Record] = 
+        val hasNextRight: Boolean = rightIteratorSorted.hasNext
+        val rightElement: Option[Data] = 
             if (hasNextRight) {
-                val element: Record = RightIteratorSorted.next()
+                val element: Data = rightIteratorSorted.next()
                 Some(element)
             } else {
                 None
@@ -241,15 +269,15 @@ class DataProcess(InputDirectories: List[String],OutputDirectory: String){
         (leftElement, rightElement) match {
             case (None, None) => Iterator()
 
-            case (None, Some(Iter)) => Iterator(Iter) ++ RightIteratorSorted
+            case (None, Some(iter)) => Iterator(iter) ++ rightIteratorSorted
 
-            case (Some(Iter), None) => Iterator(Iter) ++ LeftIteratorSorted
+            case (Some(iter), None) => Iterator(iter) ++ leftIteratorSorted
 
-            case (Some(LeftIter), Some(RightIter)) =>
-                if (LeftIter.key < RightIter.key)
-                    Iterator(LeftIter) ++ ConquerPart(LeftIteratorSorted, Iterator(RightIter) ++ RightIteratorSorted)
+            case (Some(leftIter), Some(rightIter)) =>
+                if (leftIter.key < rightIter.key)
+                    Iterator(leftIter) ++ ConquerPart(leftIteratorSorted, Iterator(rightIter) ++ rightIteratorSorted)
                 else
-                    Iterator(RightIter) ++ ConquerPart(Iterator(LeftIter) ++ LeftIteratorSorted, RightIteratorSorted)
+                    Iterator(rightIter) ++ ConquerPart(Iterator(leftIter) ++ leftIteratorSorted, rightIteratorSorted)
         }
     }
 }
